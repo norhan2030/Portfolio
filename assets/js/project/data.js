b4a296e0d581173f80e6fb351dcaf1ce130fdbee
norhan2030 <105121297+norhan2030@users.noreply.github.com>
2024-06-01 15:36:23 +0200

    nour update

--- assets/js/project/data.js
+++ assets/js/project/data.js
@@ -2,8 +2,8 @@ let prjectsDB = [
   {
     id:1,
     name: "FreashCart",
-    img: '1.PNG',
-    imgs: ['1.PNG','2.PNG','3.PNG','4.PNG','5.PNG','6.PNG','7.PNG','8.PNG','9.PNG','10.PNG','11.PNG'],
+    img: '1.png',
+    imgs: ['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png','11.png'],
     tools:['Angular','TypeScript','Bootstrapt','API','HTML','CSS','JavaScript'],
     desc: `FreashCart website, a dynamic e-commerce platform, showcasing my proficiency in HTML, CSS, JavaScript, Bootstrap, and
     Angular. This project involved integrating Route external APIs, utilizing essential Angular features such as interfaces, routing,
@@ -17,8 +17,8 @@ let prjectsDB = [
   {
     id:2,
     name: "Banking System",
-    img: '23.PNG',
-    imgs: ['21.PNG','22.PNG','23.PNG','24.PNG','25.PNG','26.PNG','27.PNG','28.PNG'],
+    img: '23.png',
+    imgs: ['21.png','22.png','23.png','24.png','25.png','26.png','27.png','28.png'],
     tools:['Angular','TypeScript','Bootstrapt','API','HTML','CSS','JavaScript'],
     desc: `Banking System, admin can adding and deleting users, creating and closing accounts, and accessing account balances.
     User can initiating fund transfers to other accounts and maintaining a transaction history. Skilled in viewing account balances and transaction records for comprehensive financial monitoring
@@ -31,7 +31,7 @@ let prjectsDB = [
     id:3,
     name: "E-Comerce",
     img: '133.png',
-    imgs: ['131.PNG','132.PNG','133.PNG','134.PNG','135.PNG','136.PNG'],
+    imgs: ['131.png','132.png','133.png','134.png','135.png','136.png'],
     tools:["JavaScript",  "Bootstrap","LocalStorge","HTML",  "CSS"],
     desc:"This website relies entirely on JavaScript and its structure to save data as objects in the local storage.",
     gith: 'https://github.com/norhan2030/E-commerce',
@@ -54,8 +54,8 @@ let prjectsDB = [
   {
     id:5,
     name: "Spatial Design",
-    img: '41.PNG',
-    imgs: ['41.PNG','42.PNG','43.PNG','45.PNG','46.PNG','47.PNG'],
+    img: '41.png',
+    imgs: ['41.png','42.png','43.png','45.png','46.png','47.png'],
     tools:["JavaScript", "HTML",  "CSS","localstorage"],
     desc:"Simple spatial design website relies entirely on JavaScript",
     gith: 'https://github.com/norhan2030/Project-JS',
@@ -66,8 +66,8 @@ let prjectsDB = [
   {
     id:6,
     name: "Route Bookmarker",
-    img: '51.PNG',
-    imgs: ['51.PNG','52.PNG'],
+    img: '51.png',
+    imgs: ['51.png','52.png'],
     tools:["javaScript",  "Bootstrap","HTML",  "CSS","localstorage"],
     desc:"Simple website that save name and url of websites that you Visit it before and you can updete and deldete",
     gith: 'https://github.com/norhan2030/Bookmarker_route',
@@ -88,8 +88,8 @@ let prjectsDB = [
   {
     id: 8,
     name: "Quotes",
-    img: '71.PNG',
-    imgs: ['71.PNG'],
+    img: '71.png',
+    imgs: ['71.png'],
     tools:["javaScript",  "HTMl","CSS",,"localstorage"],
     desc:"Simple website on  JS",
     gith: 'https://github.com/norhan2030/Qoutes-Route',
@@ -100,8 +100,8 @@ let prjectsDB = [
   {
     id: 9,
     name: "DEFELOP",
-    img: '81.PNG',
-    imgs: ['81.PNG','82.PNG','83.PNG','84.PNG','85.PNG','86.PNG','87.PNG','88.PNG'],
+    img: '81.png',
+    imgs: ['81.png','82.png','83.png','84.png','85.png','86.png','87.png','88.png'],
     tools:[  "HTMl","CSS","Bootstrap","localstorage"],
     desc:" DEFELOP website that show power of css and bootstrap",
     video: "8",
@@ -111,8 +111,8 @@ let prjectsDB = [
   {
     id: 10,
     name: "Grid System",
-    img: '91.PNG',
-    imgs: ['91.PNG','92.PNG','93.PNG'],
+    img: '91.png',
+    imgs: ['91.png','92.png','93.png'],
     tools:[  "HTMl","CSS","CSS grid"],
     desc:"This website show the power of CSS grid  ",
     // video: "9",
@@ -122,8 +122,8 @@ let prjectsDB = [
   {
     id: 11,
     name: "Mealify",
-    img: '101.PNG',
-    imgs: ['101.PNG','102.PNG','103.PNG','104.PNG','105.PNG','106.PNG'],
+    img: '101.png',
+    imgs: ['101.png','102.png','103.png','104.png','105.png','106.png'],
     tools:[  "HTMl","Css"],
     desc:"This website show the power of CSS flex ",
     video: "10",
@@ -133,8 +133,8 @@ let prjectsDB = [
   {
     id: 12,
     name: "Fokir",
-    img: '111.PNG',
-    imgs: ['111.PNG','112.PNG','113.PNG','114.PNG','115.PNG','116.PNG','117.PNG','118.PNG'],
+    img: '111.png',
+    imgs: ['111.png','112.png','113.png','114.png','115.png','116.png','117.png','118.png'],
     tools:[  "JS" ,"HTMl","Css",],
     desc:"This website show the power of CSS flex",
   
@@ -143,8 +143,8 @@ let prjectsDB = [
   {
     id: 13,
     name: "Bakery",
-    img: '121.PNG',
-    imgs: ['121.PNG','122.PNG','123.PNG','124.PNG','125.PNG','126.PNG','127.PNG','128.PNG','129.PNG','1210.PNG','1211.PNG','1212.PNG','1213.PNG','1214.PNG','1215.PNG','1216.PNG','1217.PNG'],
+    img: '121.png',
+    imgs: ['121.png','122.png','123.png','124.png','125.png','126.png','127.png','128.png','129.png','1210.png','1211.png','1212.png','1213.png','1214.png','1215.png','1216.png','1217.png'],
     tools:["HTML",  "CSS",'Bootstrap'],
     desc:"Simple website with HTML and CSS",
     filter:"others"
